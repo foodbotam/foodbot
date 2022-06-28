214917244a02359d70638865aabf5cf0e8619d5c
foodbotam <foodbotam@gmail.com>
2022-06-29 01:40:29 +0400

    initial commit

--- website/app.js
+++ website/app.js
@@ -14,82 +14,7 @@ const ap = "553225726ea919e57e3d61bcaf4a1b24"
 
 var admin_loggedin = false;
 var language = "am";
-var translated = {
-	"am" : {
-		"home" : "գլխավոր",
-		"about_project" : "նախագծի մասին",
-		"products" : "պրոդուկտներ",
-		"device_control" : "սարքավորման կառավորում",
-		"logout" : "ելք",
-		"login" : "մուտք",
-		"connect" : "միանալ",
-		"device_id" : "սարքավորման այդի",
-		"password" : "գաղտնաբառ",
-		"login_error" : "Սարքավորման այդին կամ գաղտնաբառը սխալ է",
-		"aboutus" : "մեր մասին",
-		"contactus" : "կապնվել մեզ հետ",
-		"help" : "օգնություն",
-		"update" : "թարմացնել",
-		"bread" : "հաց",
-		"cucumber" : "վարունգ",
-		"tomato" : "լոլիկ",
-		"sausage" : "երշիկ",
-		"choose_another_food" : "Ընտրել այլ ուտելիք",
-		"another" : "այլ",
-		"ketchup" : "կոտչուպ",
-		"mayonnaise" : "մայոնեզ",
-		"admin_login" : "մուտք ադմին վահանակ",
-		"admin_username" : "ադմինի մուտքանուն",
-		"admin_password" : "ադմինի գաղտնաբառ",
-		"admin" : "ադմին",
-		"new_device" : "նոր սարքավորում",
-		"add" : "ավելացնել",
-		"back" : "հետ",
-		"admin_login_error" : "Գաղտնաբառը կամ մուտքանունը սխալ է",
-		"success_device_add" : "Նոր սարքավորումը հաջողությամբ ավելացվեց",
-		"container" :"տարրա",
-		"cheese" : "պանիր",
-		"onion" : "սոխ",
-		"green_pepper" : "կանաչ բիբար"
-	},
-	"en" : {
-		"home" : "home",
-		"about_project" : "about project",
-		"products" : "products",
-		"device_control" : "device control",
-		"logout" : "logout",
-		"login" : "login",
-		"connect" : "connect",
-		"device_id" : "device id",
-		"password" : "password",
-		"login_error" : "Device id or password is wrong",
-		"aboutus" : "about us",
-		"contactus" : "contact us",
-		"help" : "help",
-		"update" : "update",
-		"bread" : "bread",
-		"cucumber" : "cucumber",
-		"tomato" : "tomato",
-		"sausage" : "sausage",
-		"choose_another_food" : "Choose another food",
-		"another" : "another",
-		"ketchup" : "ketchup",
-		"mayonnaise" : "mayonnaise",
-		"admin_login" : "login admin panel",
-		"admin_username" : "admin username",
-		"admin_password" : "admin password",
-		"admin" : "admin",
-		"new_device" : "new device",
-		"add" : "add",
-		"back" : "back",
-		"admin_login_error" : "Password or username is wrong",
-		"success_device_add" : "New device has been added successfully",
-		"container" :"container",
-		"cheese" : "cheese",
-		"onion" : "onion",
-		"green_pepper" : "green pepper"
-	}
-}
+var translated = JSON.parse(fs.readFileSync("translation.json"));
 
 // Defining server proparties
 app.set("view engine", "ejs");
@@ -116,12 +41,12 @@ function make_json_file(name, password) {
 {
 	"password" : "${md5(password)}",
 	"type" : "sandwich",
-	"container_1" : {"name" : {"en" : "bread", "am" : "հաց"} , "image_path" : "images/sandwich/bread.png"},
-	"container_2" : {"name" : {"en" : "tomato", "am" : "լոլիկ"}, "image_path" : "images/sandwich/tomato.png"},
-	"container_3" : {"name" : {"en" : "cucumber", "am" : "վարունգ"}, "image_path" : "images/sandwich/cucumber.png"},
-	"container_4" : {"name" : {"en" : "sausage", "am" : "երշիկ"}, "image_path" : "images/sandwich/sausage.png"},
-	"container_5" : {"name" : {"en" : "ketchup", "am" : "կետչուպ"}, "image_path" : "images/sandwich/ketchup.png"},
-	"container_6" : {"name" : {"en" : "mayonnaise", "am" : "մայոնեզ"}, "image_path" : "images/sandwich/mayonnaise.png"},
+	"container_1" : {"name" : "bread" , "image_path" : "images/sandwich/bread.png"},
+	"container_2" : {"name" : "tomato", "image_path" : "images/sandwich/tomato.png"},
+	"container_3" : {"name" : "cucumber", "image_path" : "images/sandwich/cucumber.png"},
+	"container_4" : {"name" : "sausage", "image_path" : "images/sandwich/sausage.png"},
+	"container_5" : {"name" : "ketchup", "image_path" : "images/sandwich/ketchup.png"},
+	"container_6" : {"name" : "mayonnaise", "image_path" : "images/sandwich/mayonnaise.png"},
 	"container_7" : "salt",
 	"container_8" : "pepper"
 }
@@ -134,17 +59,22 @@ function make_json_file(name, password) {
 function edit_container(container, newname, device_id){
 	let json_file_path = `public/devices/${md5(device_id)}.json`
 	let json_data =  JSON.parse(fs.readFileSync(json_file_path));
-	if (newname=="cucumber" || newname=="sausage" || newname=="tomato" || newname=="cheese" || newname=="onion" || newname=="green_pepper") {
-		json_data[container]["name"]['en'] = newname
-		json_data[container]["name"]['am'] = translated["am"][newname]
+	if (newname=="cucumber" || newname=="sausage" || newname=="tomato" || newname=="cheese" || newname=="onion" || newname=="green_pepper" || newname=="ketchup" || newname=="mayonnaise" || newname=="mustard") {
+		json_data[container]["name"] = newname
+		json_data[container]["type"] = "special"
 		json_data[container]["image_path"] = `images/sandwich/${newname}.png`
 		let jsonContent = JSON.stringify(json_data);
 		fs.writeFile(json_file_path, jsonContent, "utf8", function (err) { });
 	}
 	else {
-		json_data[container]["name"]['en'] = newname
-		json_data[container]["name"]['am'] = newname
-		json_data[container]["image_path"] = `images/sandwich/another.png`
+		json_data[container]["name"] = newname
+		json_data[container]["type"] = "other"
+		if(container=="container_1" || container=="container_2" || container=="container_3" || container=="container_4"){
+			json_data[container]["image_path"] = `images/sandwich/anotherfood.png`
+		}
+		if(container=="container_5" || container=="container_6"){
+			json_data[container]["image_path"] = `images/sandwich/anothersauce.png`
+		}
 		let jsonContent = JSON.stringify(json_data);
 		fs.writeFile(json_file_path, jsonContent, "utf8", function (err) { });
 	}
@@ -269,6 +199,10 @@ app.post("/changelanguage/:selected_language", (req, res) => {
 	else if (language=="english")
 	{
 		language = "en"
+	}
+	else if (language=="russian")
+	{
+		language = "ru"
 	}
 });
 
