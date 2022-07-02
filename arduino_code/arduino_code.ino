609ffd4b64bf6e22691297a271528e85520f2f8e
resonance <resonance@kali.kali>
2022-07-02 15:03:39 -0400

    new update

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -3,38 +3,31 @@
 
 // Defining servo pins
 //  Bread
-#define servo_1_pin 1
+#define servo_1_pin 30
 //  Tomato, sosage, cucumber
-#define servo_2_pin 2
-#define servo_3_pin 3
-#define servo_4_pin 4
+#define servo_2_pin 31
+#define servo_3_pin 32
+#define servo_4_pin 33
 //  Ketchup and mayonnaise
-#define servo_5_pin 5
-#define servo_6_pin 6
+#define servo_5_pin 34
+#define servo_6_pin 35
 // Salt and pepper
-#define servo_7_pin 7 
-#define servo_8_pin 8
+#define servo_7_pin 36
+#define servo_8_pin 37
+
+// Define method for getting array size
+#define ARRAY_SIZE(array) ((sizeof(array))/(sizeof(array[0])))
 
 // Defining ultrasonic pins
-#define ultrasonic_trig_pin 10
-#define ultrasonic_1_pin 11
-#define ultrasonic_2_pin 12
-#define ultrasonic_3_pin 13
-#define ultrasonic_4_pin 14
-#define ultrasonic_5_pin 15
-#define ultrasonic_6_pin 16
-#define ultrasonic_7_pin 17
-#define ultrasonic_8_pin 18
-
-// Defining servo motor pins
-#define servo_1_pin 1
-#define servo_2_pin 2
-#define servo_3_pin 3
-#define servo_4_pin 4
-#define servo_5_pin 5
-#define servo_6_pin 6
-#define servo_7_pin 7
-#define servo_8_pin 8
+#define ultrasonic_trig_pin 2
+#define ultrasonic_1_pin 41
+#define ultrasonic_2_pin 42
+#define ultrasonic_3_pin 43
+#define ultrasonic_4_pin 44
+#define ultrasonic_5_pin 45
+#define ultrasonic_6_pin 46
+#define ultrasonic_7_pin 47
+#define ultrasonic_8_pin 48
 
 // Defining container variables
 int container_1_length = 30;
@@ -220,21 +213,21 @@ void cut_food(int number)
   switch(number) {
   case 2:
     servo_2.write(180);
-    delay(1000);
+    delay(2000);
     servo_2.write(0);
-    delay(1000);
+    delay(2000);
     break;
   case 3:
     servo_3.write(180);
-    delay(1000);
+    delay(2000);
     servo_3.write(0);
-    delay(1000);
+    delay(2000);
     break;
   case 4:
     servo_4.write(180);
-    delay(1000);
+    delay(2000);
     servo_4.write(0);
-    delay(1000);
+    delay(2000);
     break;
   default:
     break;
@@ -320,20 +313,40 @@ void setup() {
   // Starting serial monitor for communication
   Serial.begin(115200);
   // Reseting servos
-  Serial.print("Sksuma");
   reset_servos();
-  Serial.println("Sksav");
 }
+
+void make_from_list(int components_list[])
+{
+  Serial.print("sizeof(components_list");
+  Serial.println(sizeof(components_list));
+    for(int i = 0; i < sizeof(components_list); i++)
+    {
+      Serial.println(components_list[i]);
+    }
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  Serial.println("Check started" );
-//  update_containers_status();
-//  Serial.print("Status: ");
-//  Serial.println(container_2_status);
-//  Serial.println("Check finished" );
-//  delay(200);
-    delay(2000);
-    cut_food(2);
-    delay(2000);
+ int components[4] = {1,2,3,4};
+ make_from_list(components);
+// Serial.println("Check started" );
+// update_containers_status();
+// Serial.print("Status: ");
+// Serial.println(container_2_status);
+// Serial.println("Check finished" );
+// delay(200);
+  int MyArray[4];
+  MyArray[0] = 2419;
+  MyArray[1] = 24;
+  MyArray[2] = 19;
+  MyArray[3] = 9;
+  
+  make_from_list(MyArray);
+
+
+  
+     drop_bread();
+     delay(2000);
 }
